6698d61612acd201a39961002357c979a5dc94e7
Himanshu Kumar <subodhkumar321.1@gmail.com>
2024-03-23 02:24:41 +0530

    ut

--- src/app/api/updateTier/route.ts
+++ src/app/api/updateTier/route.ts
@@ -10,56 +10,64 @@ import Razorpay from "razorpay";
 import crypto from "crypto";
 export async function POST(request: Request) {
   const formData = await request.formData();
-  const razorpayOrderId = formData.get("razorpayOrderId");
-  const razorpayPaymentId = formData.get("razorpayOrderId");
-  const razorpaySignature = formData.get("razorpayOrderId");
+  const razorpayOrderId = formData.get("razorpay_order_id");
+  const razorpayPaymentId = formData.get("razorpay_payment_id");
+  const razorpaySignature = formData.get("razorpay_signature");
   const cookieStore = cookies();
   const token: RequestCookie | undefined = cookies().get("token");
   const user: any = jwt.verify(token.value, process.env.JWT_KEY);
   console.log(user);
 
   const body = razorpayOrderId + "|" + razorpayPaymentId;
+  const currentDateTime = new Date();
+  const formattedDateTime = currentDateTime.toLocaleString();
 
   const expectedSignature = crypto
     .createHmac("sha256", "3UFrNGkdLR9apMa3dOUE1jvh")
     .update(body.toString())
     .digest("hex");
   const isAuthentuic = razorpaySignature === expectedSignature;
+  let dataToInsert = {
+    razorpayOrderId: razorpayOrderId,
+    razorpayPaymentId: razorpayPaymentId,
+    razorpaySignature: razorpaySignature,
+    amount: 500,
+    transactionTime: formattedDateTime,
+    userId: user.userId
+  }
 
-  // if (isAuthentuic) {
-  //   try {
-  //     await client.connect();
-  //     const currentDateTime = new Date();
-  //     const formattedDateTime = currentDateTime.toLocaleString();
-  //     console.log("Connected successfully to server");
-  //     const db = client.db(dbName);
-  //     const collection = db.collection("payment");
+  if (isAuthentuic) {
 
-  //     const updateResult = await collection.findOneAndUpdate(
-  //       { userId: user.userId },
-  //       { $set: { tier: "paid", updatedOn: formattedDateTime } },
-  //       { returnDocument: "after" } // Return the updated document
-  //     );
-
-  //     // Check if the update was successful
-  //     if (updateResult) {
-  //       // Return the updated document as JSON
-  //       return Response.json({ success: true, updateResult: updateResult });
-  //     } else {
-  //       // Return an error message
-  //       return Response.json({ success: false, message: "Update failed" });
-  //     }
-  //   } catch (err: any) {
-  //     // Handle any errors
-  //     console.error(err);
-  //     return Response.json({ success: false, message: err.message });
-  //   } finally {
-  //     // Close the connection
-  //     await client.close();
-  //   }
-  // } else {
-  //   return Response.json({ success: false });
-  // }
+    try {
+      await client.connect();
+      console.log("Connected successfully to server");
+      const db = client.db(dbName);
+      const transaction = db.collection("transaction");
+      const userCollection = db.collection("user")
+      const insertResult = await transaction.insertOne(dataToInsert);
+      const updateResult = await userCollection.findOneAndUpdate(
+        { userId: user.userId },
+        { $set: { tier: "paid", updatedOn: formattedDateTime } },
+      )
+      // Check if the update was successful
+      if (updateResult) {
+        // Return the updated document as JSON
+        return Response.json({ success: true, updateResult: updateResult });
+      } else {
+        // Return an error message
+        return Response.json({ success: false, message: "Update failed" });
+      }
+    } catch (err: any) {
+      // Handle any errors
+      console.error(err);
+      return Response.json({ success: false, message: err.message });
+    } finally {
+      // Close the connection
+      await client.close();
+    }
+  } else {
+    return Response.json({ success: false });
+  }
 
   // var instance = new Razorpay({
   //   key_id: "rzp_test_gKANZdsNdLqaQs",
@@ -79,5 +87,5 @@ export async function POST(request: Request) {
   // console.log(formData.get("razorpay_payment_id"));
   redirect("/profile");
 
-  return Response.json({ });
+  return Response.json({});
 }
