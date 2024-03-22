8feb72f96ceb119c49d9ebe776160167485fec93
Himanshu Kumar <subodhkumar321.1@gmail.com>
2024-03-22 20:14:37 +0530

    Update route.ts

--- src/app/api/updateTier/route.ts
+++ src/app/api/updateTier/route.ts
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
 import { redirect } from 'next/navigation'
+import { useAppSelector } from "../redux/store";
 const url = process.env.MONGO_URL;
 const client = new MongoClient(url);
 const dbName = process.env.MONGO_DB;
@@ -7,8 +8,10 @@ const dbName = process.env.MONGO_DB;
 export async function POST(request: Request) {
 const formData = await request.formData()
   let response: string="test";
+  console.log(formData.get("razorpay_payment_id"))
+  const userInfo = useAppSelector((state) => state.user);
+  console.log(userInfo)
   redirect('/profile')
-console.log(formData.get("razorpay_payment_id"))
 
   return Response.json({response});
 }
